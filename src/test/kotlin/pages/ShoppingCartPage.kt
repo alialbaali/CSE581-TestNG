8263084c3d90ddb4ed234a4e9b9f2044568d1cf7
Ali Albaali <ali@albaali.com>
2020-12-05 18:36:31 +0300

    Add ShoppingCartPage

---
+++ src/test/kotlin/pages/ShoppingCartPage.kt
@@ -0,0 +1,34 @@
+package pages
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
+class ShoppingCartPage(private val driver: WebDriver) {
+
+    val navbar = Navbar(driver)
+
+    private val continueShoppingButton = driver.findElement(By.linkText("CONTINUE SHOPPING"))
+    private val checkoutButton = driver.findElement(By.linkText("CHECKOUT"))
+
+    fun getCartItems(): List<WebElement> {
+        return driver.findElements(By.className("cart_item"))
+    }
+
+    fun removeCartItem(index: Int) {
+        getCartItems()[index]
+            .findElement(By.className("cart_button"))
+            .click()
+    }
+
+    fun checkout(): CheckoutPage.CheckoutStep1 {
+        checkoutButton.click()
+        return CheckoutPage(driver).CheckoutStep1()
+    }
+
+    fun continueShopping(): InventoryPage {
+        continueShoppingButton.click()
+        return InventoryPage(driver)
+    }
+
+}
